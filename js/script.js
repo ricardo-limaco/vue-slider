a49092ac3324ef349818e163fd95ca6447a28f54
ricardo-limaco <ricardolimaco.05@gmail.com>
2021-10-29 16:34:20 +0200

    Printed Sidebar

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ Vue.config.devtools = true;
 new Vue({
   el: `#root`,
   data: {
-      imageList: [
+    imageList: [
         {
           url:`img/01.jpg`,
           title:`Svezia`,
@@ -29,8 +29,29 @@ new Vue({
           title:`Paradise`,
           text:`Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.`,
         },
-      ]
-  }
+      ],
+    currentActiveImage: 0,
+  },
+  methods: {
+    arrowUp () {
+      let item = this.currentActiveImage - 1;
+
+      if (item < 0) {
+        item = this.imageList.lenght - 1;
+      }
+
+      this.currentActiveImage = item
+    },
+    arrowDown () {
+      let item = this.currentActiveImage + 1;
+
+      if (item > this.imageList.lenght - 1) {
+        item = 0;
+      }
+
+      this.currentActiveImage = item
+    },
+  },
 });
 
 
